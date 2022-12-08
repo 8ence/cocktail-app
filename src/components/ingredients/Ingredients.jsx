2b04a40d7e63244038ac6412f26b324c97247b37
8ence <kovaribence95@gmail.com>
2022-12-08 10:00:41 +0100

    fixed no-data

--- src/components/ingredients/Ingredients.jsx
+++ src/components/ingredients/Ingredients.jsx
@@ -6,9 +6,9 @@ export const Ingredients = ({ ingredient, fromIngredient, setDrink }) => {
   return (
     <div className="from-ingredient-container">
       <h4>Check what u can do with {ingredient}</h4>
-      {fromIngredient &&
-        fromIngredient.map((drink) => (
-          <div className="from-ingredient-card">
+      {/* {fromIngredient &&
+        fromIngredient.map((drink, index) => (
+          <div className="from-ingredient-card" key={index}>
             <div>
               <h3>{drink.strDrink}</h3>
               <Button
@@ -21,7 +21,29 @@ export const Ingredients = ({ ingredient, fromIngredient, setDrink }) => {
             </div>
             <img src={drink.strDrinkThumb} alt="" />
           </div>
-        ))}
+        ))} */}
+      {fromIngredient && fromIngredient !== "None Found" ? (
+        <div className="from-ingredient-card-container">
+          {fromIngredient &&
+            fromIngredient.map((drink, index) => (
+              <div className="from-ingredient-card" key={index}>
+                <div>
+                  <h3>{drink.strDrink}</h3>
+                  <Button
+                    variant="contained"
+                    onClick={() => {
+                      setDrink(drink.strDrink);
+                    }}>
+                    Show more
+                  </Button>
+                </div>
+                <img src={drink.strDrinkThumb} alt="" />
+              </div>
+            ))}
+        </div>
+      ) : (
+        <div>Nincs hozzá pia</div>
+      )}
     </div>
   );
 };
